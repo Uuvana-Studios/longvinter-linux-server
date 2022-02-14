e2a1271a87ff23f03c4eb124f8938949d599ded3
jalsson <nilsson.niklas.97@gmail.com>
2022-02-14 12:38:02 +0200

    init

---
+++ LongvinterServer.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+UE4_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
+UE4_PROJECT_ROOT=$(dirname "$UE4_TRUE_SCRIPT_NAME")
+chmod +x "$UE4_PROJECT_ROOT/Longvinter/Binaries/Linux/LongvinterServer-Linux-Shipping"
+"$UE4_PROJECT_ROOT/Longvinter/Binaries/Linux/LongvinterServer-Linux-Shipping" Longvinter "$@" 
