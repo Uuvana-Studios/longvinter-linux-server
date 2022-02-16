ea58429810f1f23ae4b42e8bd82e5b7b220e43f8
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 <nilsson@longvinter-server-us-2.us-central1-a.c.uams-vm.internal>
2022-02-16 07:26:05 +0000

    server autorun

---
+++ server-autorun.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+echo checking for updates
+
+UpdateServer () {
+	killall LongvinterServer.sh
+        echo "Server closed, now getting latest version"
+        sudo git restore .
+        sudo git pull "https://jalsson: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@github.com/Uuvana-Studios/longvinter-linux-server.git" main
+        sleep 1
+        echo "Starting server.."
+        sudo chmod -R ugo+rwx /home/nilsson/longvinter-linux-server/
+        nohup sh ~/longvinter-linux-server/LongvinterServer.sh > ~/server-logs/longvinter.log &
+}
+
+cd ~/longvinter-linux-server
+
+git fetch
+
+if git merge-base --is-ancestor origin/main main ; then
+        echo "Nothing to update"
+else
+        echo "Change found, updating server.."
+        UpdateServer
+fi
