01f4e66f39694908df7899bf3004623cedde9d8a
jalsson <nilsson.niklas.97@gmail.com>
2022-02-24 13:07:11 +0200

    fixed crashcheck

--- server-crashCheck.sh
+++ server-crashCheck.sh
@@ -3,7 +3,7 @@
 echo checking server status
 
 
-pidof  sh /home/nilsson/longvinter-linux-server/LongvinterServer.sh
+pidof bash  ~/longvinter-linux-server/LongvinterServer.sh
 if [[ $? -ne 0 ]] ; then
 	bash ~/longvinter-linux-server/LongvinterServer.sh
         echo "Restarting script"
