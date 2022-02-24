c48f24400c2da927ef47187c23e48722decdb4a2
jalsson <nilsson.niklas.97@gmail.com>
2022-02-24 14:47:51 +0200

    adding server crash

--- server-crashCheck.sh
+++ server-crashCheck.sh
@@ -4,6 +4,7 @@ echo checking server status
 
 
 pidof bash  ~/longvinter-linux-server/LongvinterServer.sh
+echo $?
 if [[ $? -le 2 ]] ; then
 	bash ~/longvinter-linux-server/LongvinterServer.sh
         echo "Restarting script"
