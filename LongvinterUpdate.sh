d876db7bfc8456703df7815b8a9d408623c24a54
Artur Mancha <joker@nadaradical.com>
2022-03-07 16:52:43 +0000

    Sudo on required commands

--- LongvinterUpdate.sh
+++ LongvinterUpdate.sh
@@ -3,14 +3,14 @@ echo "Checking for updates..."
 
 UpdateServer () {
         systemctl is-active --quiet longvinter && echo "Server is running!"
-        systemctl stop longvinter && echo "Server is now closed, now getting latest version..."
+        sudo systemctl stop longvinter && echo "Server is now closed, now getting latest version..."
         git restore .
         sleep 1
         git pull "https://github.com/Uuvana-Studios/longvinter-linux-server.git" main
         sleep 1
         echo "Starting server..."
         sudo chmod -R ugo+rwx /home/steam/longvinter-linux-server/
-        systemctl start longvinter && echo "Server is now running!"
+        sudo systemctl start longvinter && echo "Server is now running!"
 }
 
 cd /home/steam/longvinter-linux-server
