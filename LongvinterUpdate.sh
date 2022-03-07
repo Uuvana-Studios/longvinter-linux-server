0c178d2d1a9415e48d7770c3ed2ed028e1622cce
jalsson <nilsson.niklas.97@gmail.com>
2022-03-07 16:25:29 +0200

    also reverted update script

--- LongvinterUpdate.sh
+++ LongvinterUpdate.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-echo "Checking for updates..."
+echo checking for updates
 
 UpdateServer () {
-        systemctl is-active --quiet longvinter && echo "Server is running!"
-        systemctl stop longvinter && echo "Server is now closed, now getting latest version..."
+	killall LongvinterServer.sh
+        echo "Server closed, now getting latest version"
         git restore .
         sleep 1
         git pull "https://github.com/Uuvana-Studios/longvinter-linux-server.git" official
         sleep 1
-        echo "Starting server..."
-        sudo chmod -R ugo+rwx /home/nilsson/longvinter-linux-server/
-        systemctl start longvinter && echo "Server is now running!"
+        echo "Starting server.."
+        sudo chmod -R ugo+rwx ~/longvinter-linux-server/
+        bash ~/longvinter-linux-server/LongvinterServer.sh
 }
 
-cd /home/nilsson/longvinter-linux-server
+cd ~/longvinter-linux-server
 
 git fetch
 
-if git merge-base --is-ancestor origin/main main ; then
-        echo "Nothing to update."
+if git merge-base --is-ancestor origin/official official ; then
+        echo "Nothing to update"
 else
-        echo "Change found, updating server..."
+        echo "Change found, updating server.."
         UpdateServer
 fi
