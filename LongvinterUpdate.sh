d10942d7bb3e63fe27e9b213a41904d9ed1758e9
Artur Mancha <joker@nadaradical.com>
2022-03-07 11:19:26 +0000

    Improved update script

---
+++ LongvinterUpdate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Checking for updates..."
+
+UpdateServer () {
+        systemctl is-active --quiet longvinter && echo "Server is running!"
+        systemctl stop longvinter && echo "Server is now closed, now getting latest version..."
+        git restore .
+        sleep 1
+        git pull "https://github.com/Uuvana-Studios/longvinter-linux-server.git" main
+        sleep 1
+        echo "Starting server..."
+        sudo chmod -R ugo+rwx /home/steam/longvinter-linux-server/
+        systemctl start longvinter && echo "Server is now running!"
+}
+
+cd /home/steam/longvinter-linux-server
+
+git fetch
+
+if git merge-base --is-ancestor origin/main main ; then
+        echo "Nothing to update."
+else
+        echo "Change found, updating server..."
+        UpdateServer
+fi
