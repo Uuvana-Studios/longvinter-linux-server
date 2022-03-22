639a3654805d4652dc6a44c7dac35537f8c7e9ad
WarpWing <professornitro@gmail.com>
2022-03-22 11:24:47 -0400

    Added friendly message

--- LongvinterGetId.sh
+++ LongvinterGetId.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 hexaKey=$(grep -Eo -m 1 "0[xX][0-9a-fA-F]{15}+" Longvinter/Saved/Logs/Longvinter.log)
 hexaKey=${hexaKey: 2}
-echo $(( 16#$hexaKey ))
+echo Your server ID is $(( 16#$hexaKey ))
