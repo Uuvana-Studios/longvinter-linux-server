57d48473915aed306d30ac37d27ea13930f33500
jalsson <nilsson.niklas.97@gmail.com>
2026-04-16 21:01:20 +0300

    Removed server files

--- LongvinterServer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-UE_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
-UE_PROJECT_ROOT=$(dirname "$UE_TRUE_SCRIPT_NAME")
-chmod +x "$UE_PROJECT_ROOT/Longvinter/Binaries/Linux/LongvinterServer-Linux-Shipping"
-"$UE_PROJECT_ROOT/Longvinter/Binaries/Linux/LongvinterServer-Linux-Shipping" Longvinter "$@" 
